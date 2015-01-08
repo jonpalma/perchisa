2b862e3d51bbaedf7b29f24e3092b80127d5fdc8
jonpalma <jon.palma@mixen.mx>
2015-01-08 11:01:34 -0700

    Maquinaria listo
    Ya quedo el grid de el área de maquinaria, se encuentra dentro de la
    clase <div class=¨col-lg-6¨>…<//div>

--- html/maquinaria-en-venta.php
+++ html/maquinaria-en-venta.php
@@ -34,63 +34,76 @@
               <div class="icon-b"><img src="img/icon-b.png" class="img-responsive" alt=""></div>
                <h2 style="text-align:center;"><strong>MAQUINARIA EN VENTA</strong></h2>
               <hr>
-              <div class="col-lg-3"><br><img src="img/venta/spencer-harris.jpg" class="img-circle img-responsive"  alt="Empresa PERCHISA Perforaciones en Chihuahua SA de CV"></div>
-              <div class="col-lg-3">
-                <h4>SPENCER HARRIS</h4>
-                <table class="table">
-                  <tr>
-                    <td>Marca:</td>
-                    <td>Maquina Spencer Harris </td>
-                  </tr>
-                  <tr>
-                    <td>Modelo:</td>
-                    <td>6000</td>
-                  </tr>
-                  <tr>
-                    <td>Descripción:</td>
-                    <td>
-                      Capacidad de 250,000 lbs.<br>
-                      Para 6000 pies .<br>
-                      Motor 8v 92 detrot diesel.<br>
-                      Torre de 110 pies de doble sección.<br>
-                      Malacate de un solo tambor mesa rotaria de 17 1/2.<br>
-                      Freno hidromatico.<br>
-                      Bomba de lodo gardner denver de 7 1/2" x 12.<br>
-                      kelly cuadrado de 4 1/4.<br>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Precio:</td>
-                    <td>$ - - - -</td>
-                  </tr>
-                </table>
+              
+              <div class="col-lg-6">
+                  <div class="col-lg-6 col-md-6 col-sm-3"><br>
+                      <img src="img/venta/spencer-harris.jpg" class="img-circle img-responsive"  alt="Empresa PERCHISA Perforaciones en Chihuahua SA de CV">
+                  </div>
+                  <div class="col-lg-6 col-md-6 col-sm-3">
+                    <h4>SPENCER HARRIS</h4>
+                    <table class="table">
+                      <tr>
+                        <td>Marca:</td>
+                        <td>Maquina Spencer Harris </td>
+                      </tr>
+                      <tr>
+                        <td>Modelo:</td>
+                        <td>6000</td>
+                      </tr>
+                      <tr>
+                        <td>Descripción:</td>
+                        <td>
+                          Capacidad de 250,000 lbs.<br>
+                          Para 6000 pies .<br>
+                          Motor 8v 92 detrot diesel.<br>
+                          Torre de 110 pies de doble sección.<br>
+                          Malacate de un solo tambor mesa rotaria de 17 1/2.<br>
+                          Freno hidromatico.<br>
+                          Bomba de lodo gardner denver de 7 1/2" x 12.<br>
+                          kelly cuadrado de 4 1/4.<br>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>Precio:</td>
+                        <td>$ - - - -</td>
+                      </tr>
+                    </table>
+                  </div>
               </div>
-              <div class="col-lg-3"><br><img src="img/venta/ingersoll-rand.jpg" class="img-circle img-responsive"  alt="Empresa PERCHISA Perforaciones en Chihuahua SA de CV"></div>
-              <div class="col-lg-3">
-                <h4>COMPRESOR INGERSOLL RAND</h4>
-                <table class="table">
-                  <tr>
-                    <td>Marca:</td>
-                    <td>INGERSOLL RAND</td>
-                  </tr>
-                  <tr>
-                    <td>Modelo:</td>
-                    <td>8V92</td>
-                  </tr>
-                  <tr>
-                    <td>Descripción:</td>
-                    <td>Compresor Ingresoll Rand montado en patin <br>
-                      950 CFM <br>
-                      350 PSI <br>
-                      Motor Detroit Diesel <br>
-                      8V92</td>
-                  </tr>
-                  <tr>
-                    <td>Precio:</td>
-                    <td>$ - - - -</td>
-                  </tr>
-                </table>
+              
+              
+              <div class="col-lg-6">
+                  <div class="col-lg-6 col-md-6 col-sm-3"><br>
+                      <img src="img/venta/ingersoll-rand.jpg" class="img-circle img-responsive"  alt="Empresa PERCHISA Perforaciones en Chihuahua SA de CV">
+                  </div>
+                  <div class="col-lg-6 col-md-6 col-sm-3">
+                    <h4>COMPRESOR INGERSOLL RAND</h4>
+                    <table class="table">
+                      <tr>
+                        <td>Marca:</td>
+                        <td>INGERSOLL RAND</td>
+                      </tr>
+                      <tr>
+                        <td>Modelo:</td>
+                        <td>8V92</td>
+                      </tr>
+                      <tr>
+                        <td>Descripción:</td>
+                        <td>Compresor Ingresoll Rand montado en patin <br>
+                          950 CFM <br>
+                          350 PSI <br>
+                          Motor Detroit Diesel <br>
+                          8V92</td>
+                      </tr>
+                      <tr>
+                        <td>Precio:</td>
+                        <td>$ - - - -</td>
+                      </tr>
+                    </table>
+                  </div>
               </div>
+              
+              
             </div>
           </div>
       </div>
